661d63452ea4b6e96c81809efd1eff70876d0eba
Madhu HS <madhu.s.ty@cars24.com>
2025-11-14 11:23:04 +0530

    adding hbm.xml

---
+++ src/main/java/com/jspiders/ConnectionDemo.java
@@ -0,0 +1,38 @@
+package com.jspiders;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class ConnectionDemo
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Program starts...");
+
+        //load configuration
+        Configuration config = new Configuration();
+        config.configure("hibernate.cfg.xml");
+
+        //build session factory
+        SessionFactory sessionFactory = config.buildSessionFactory();
+        System.out.println("sessionFactory created..");
+
+        //get session from session factory
+        Session session = sessionFactory.openSession();//connect to db
+        System.out.println("session created..");
+
+        //logics
+
+
+        //close session
+        session.close();
+        System.out.println("session closed..");
+        //close session factory
+        sessionFactory.close();
+        System.out.println("sessionFactory closed..");
+
+        System.out.println("Program ends...");
+
+    }
+}
---
+++ src/main/java/com/jspiders/Users.java
@@ -0,0 +1,40 @@
+package com.jspiders;
+
+public class Users {
+   private Long userid;
+   private String name;
+   private String mobile;
+   private String password;
+
+    public Long getUserid() {
+        return userid;
+    }
+
+    public void setUserid(Long userid) {
+        this.userid = userid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
