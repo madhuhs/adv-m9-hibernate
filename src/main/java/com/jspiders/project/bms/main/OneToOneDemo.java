29093256683e530bb585572fc44d8ca97a88c17b
Madhu HS <madhu.s.ty@cars24.com>
2025-11-19 12:51:14 +0530

    OneToOne relationship demo

--- src/main/java/com/jspiders/project/bms/main/OneToOneDemo.java
+++ src/main/java/com/jspiders/project/bms/main/OneToOneDemo.java
@@ -1,7 +1,10 @@
 package com.jspiders.project.bms.main;
 
+import com.jspiders.project.bms.Address;
+import com.jspiders.project.bms.Audi;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 public class OneToOneDemo {
@@ -18,14 +21,62 @@ public class OneToOneDemo {
         System.out.println("2.Create SessionFactory");
         sessionFactory = config.buildSessionFactory();
     }
+
+    public static void saveAudi()
+    {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+
+        System.out.println("Creating Audi data");
+        Audi a1 = new Audi();
+        a1.setName("AUDI-1");
+        a1.setSeatRows(20);
+        a1.setSeatColumns(30);
+
+        System.out.println("Creating Address data");
+        Address adr1 = new Address();
+        adr1.setStreet("Street-1");
+        adr1.setArea("HBR");
+        adr1.setCity("BLR");
+
+        System.out.println("Linking Adui to Address ");
+        a1.setAddress(adr1);//link Audi to Address
+
+        session.persist(a1);
+
+        System.out.println("Audi and Address saved!!");
+
+        transaction.commit();
+        session.close();
+        System.out.println("Session closed");
+    }
+
+    public static void deleteAudi(Long id)
+    {
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+
+        System.out.println("Finding Audi data with id : "+id);
+        Audi a1 = session.find(Audi.class,id);
+        session.remove(a1);
+        System.out.println("Audi and Address deleted!!");
+        transaction.commit();
+        session.close();
+        System.out.println("Session closed");
+    }
+
+    public static void getAudiDetails()
+    {
+
+    }
+
     public static void main(String[] args) {
 
         System.out.println("Program starts...");
 
-        Session session = sessionFactory.openSession();
+       // saveAudi();
+        deleteAudi(1l);
 
-        System.out.println("3.Close Session");
-        session.close();
         System.out.println("4.Close Session Factory");
         sessionFactory.close();
 
